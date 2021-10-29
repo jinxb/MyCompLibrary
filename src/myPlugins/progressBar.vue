5d969c79668ea6a1e1cba5ef40d057911bf74bc6
jinxb <1659340302@qq.com>
2021-10-29 18:05:19 +0800

    增加横向

--- src/myPlugins/progressBar.vue
+++ src/myPlugins/progressBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="progress-ver">
-    <div class="progress-bar">
+    <div class="progress-bar"  :style="`display: ${pType === 'across' ? 'flex' : ''}`">
       <!-- 竖向默认外显百分比，支持自定义文字 -->
       <span class="inside-top" :style="`color:${textInsideColor};`">{{ textInsideTop }}</span><span class="inside-top-suffix">{{ topSuffix }}</span>
-      <div class="progress-outer" :style="{width:strokeWidth + 'px'}">
-        <div class="progress-inner" :style="innerStyle"></div>
+      <div :class="`progress-${pType}-outer`" :style="{[pType === 'vertical' ? 'width' : 'height']:strokeWidth + 'px'}">
+        <div :class="`progress-${pType}-inner`" :style="innerStyle"></div>
       </div>
       <span class="inside-btm">{{ textInsideBtm }}</span><span class="inside-btm-suffix">{{ btmSuffix }}</span>
     </div>
@@ -28,12 +28,12 @@ export default {
       type: Number,
       default: 8
     },
-    // pType: {
-    //   // 进度条 横竖
-    //   type: String,
-    //   default: 'vertical',
-    //   validator: val => ['across', 'vertical'].includes(val)
-    // },
+    pType: {
+      // 进度条 横竖
+      type: String,
+      default: 'vertical',
+      validator: val => ['across', 'vertical'].includes(val)
+    },
     pColor: {
       // 进度条颜色
       type: String,
@@ -69,8 +69,13 @@ export default {
     // 竖向height
     innerStyle() {
     // 计算属性调用其他计算属性，必须加this关键字，否则找不到
-      console.log(this.textInsideColor)
-      return { height: 150 * (parseInt(this.percentage) / 100) + 'px', backgroundColor: this.pColor }
+      // console.log(this.textInsideColor)
+      if (this.pType === 'vertical') {
+        // 纵向进度条固定150px
+        return { height: 150 * (parseInt(this.percentage) / 100) + 'px', backgroundColor: this.pColor }
+      } else {
+        return { width:this.percentage+'%', backgroundColor: this.pColor }
+      }
     }
   },
   watch: {
@@ -99,16 +104,32 @@ export default {
   /* flex: 1; */
   text-align: center;
 }
-.progress-outer {
+.progress-vertical-outer {
     position:relative;
     margin: 5px auto;
     height:150px;
+    border-radius: 10px;
     background-color:#e5e5e5;
 }
-.progress-inner {
+.progress-vertical-inner {
     position:absolute;
     bottom:0px;
     width:100%;
+    border-radius: 10px;
     background-color:#fc5c28;
 }
+.progress-across-outer{
+    width: 100%;
+    border-radius: 10px;
+    margin: auto;
+    background-color: #e5e5e5;
+}
+.progress-across-inner{
+    background-color: #fc5c28;
+    border-radius: 10px;
+    height: 100%;
+    /* transition: width 0.6s ease; */
+    text-align: right;
+    line-height: 80%;
+   }
 </style>
